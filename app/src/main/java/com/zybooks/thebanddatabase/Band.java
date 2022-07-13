605ba8b2372a578f47c1d231fd8115a650edf2f4
Gino Murin <gmurin@gmail.com>
2022-07-13 13:48:32 -0400

    Implemented class logic

--- app/src/main/java/com/zybooks/thebanddatabase/Band.java
+++ app/src/main/java/com/zybooks/thebanddatabase/Band.java
@@ -1,4 +1,38 @@
 package com.zybooks.thebanddatabase;
 
 public class Band {
+    private int mId;
+    private String mName;
+    private String mDescription;
+
+    public Band(int id, String name, String description){
+        mId = id;
+        mName = name;
+        mDescription = description;
+    }
+
+    public int getId(){
+        return mId;
+    }
+
+    public void setId(int id){
+        this.mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        this.mDescription = description;
+    }
+
 }
--- app/src/main/java/com/zybooks/thebanddatabase/BandDatabase.java
+++ app/src/main/java/com/zybooks/thebanddatabase/BandDatabase.java
@@ -1,4 +1,42 @@
 package com.zybooks.thebanddatabase;
 
+import android.content.Context;
+import android.content.res.Resources;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BandDatabase {
+
+    private static BandDatabase sBandDatabase;
+    private List<Band> mBands;
+
+    public static BandDatabase getInstance(Context context) {
+        if (sBandDatabase == null) {
+            sBandDatabase = new BandDatabase(context);
+        }
+        return sBandDatabase;
+    }
+
+    private BandDatabase(Context context) {
+        mBands = new ArrayList<>();
+        Resources res = context.getResources();
+        String[] bands = res.getStringArray(R.array.bands);
+        String[] descriptions = res.getStringArray(R.array.descriptions);
+        for (int i = 0; i < bands.length; i++) {
+            mBands.add(new Band(i + 1, bands[i], descriptions[i]));
+        }
+    }
+
+    public List<Band> getBands() {
+        return mBands;
+    }
+
+    public Band getBand(int bandId) {
+        for (Band band : mBands) {
+            if (band.getId() == bandId) {
+                return band;
+            }
+        }
+        return null;
+    }
 }
